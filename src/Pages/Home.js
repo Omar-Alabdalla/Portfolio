310a10c2cea8fa30a2e351711b28b93755a110a9
Omar-Alabdalla <omar.alabdalla04@gmail.com>
2022-10-20 14:29:14 -0500

    Most updated version

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,59 +4,42 @@ function Home() {
   return (
     <>
       <p1>
-        My name is <f>Omar Alabdalla</f>, I am a Junior Developer hoping to
-        pursue a career in <f>Computer Science</f>.
-        <br /> Coding languages I currently work with include{" "}
-        <f>JavaScript, React, Java, C++, and Python</f>.
-        <br />
-        Areas of experience include <f>HTML</f> and <f>Web Development</f>,{" "}
-        <f>RESTful</f> coding through Scratch and <br />
-        <f>REST API </f>
-        development, application development, and
-        <f> CyberSecurity</f>. <br /> I currently am going to college part time
-        to get a degree in computer science.
-        <br /> I first began learning about coding in middle school with block
-        coding through <f>Scratch</f>
-        <br /> Although I enjoyed it I still believed that there had to be more
-        to coding than block coding.
-        <br /> I decided to learn <f>batch</f> development but this still didnt
-        satisy me so I withheld my learning until High School.
-        <br /> In High school I decided to take every programming class, and was
-        able to learn the basics of <f>Web Development</f>.
-        <br /> I also was able to learn the advanced aspects of <f>Java</f>{" "}
-        which I applied to create a game.
-        <br /> Although I learned a leading to senior year I still wasnt
-        satisfied with my progress so I created my first project.
-        <br /> When I was trying to think of what I might want to develop I
-        could only think of one thing.
-        <br />
+        Hello, my name is Omar Alabdalla! I am a Junior Developer pursuing a
+        career in Computer Science. The coding languages I’ve work with include
+        JavaScript, React, Java, C++, and Python. My areas of experience include
+        HTML and Web Development, RESTful coding through Scratch and REST API,
+        application development, and Cybersecurity. I currently attend college,
+        pursuing my degree in Computer Science. I first started learning about
+        coding in middle school with block coding through Scratch. I enjoyed my
+        time with it and so I continued from there into more advanced forms of
+        coding. I went on to learn a bit of batch development until I started
+        High School, from which I went on to take every programming class I
+        could. I was able to learn some Web Development also some advanced parts
+        of Java which I had then applied to creating my own game. Having learned
+        a lot up to senior year I was eager to learn more so I went on to start
+        my next major project. After intense brainstorming of ideas for a
+        befitting project I came to one conclusion, a Discord Bot.
       </p1>
       <p2>
-        <f> A Discord Bot.</f> I began with no prior knowledge of coding outside
-        of basic html programming and Java development.
-        <br /> However this was the part that excited me the most since it was a
-        whole new type of programming.
-        <br /> I didn't have any specific due date nor any features I had to add
-        all I had to do was make the bot.
-        <br /> After asking a few friends what I should add to the bot I decided
-        to add a feature to search weather in any zipcode.
-        <br /> At first I thought this would be a simple mater but instead it
-        ended up required a lot of high level knowledge.
-        <br /> This led me to learn <f>RESTful</f> programming which I also
-        learned how to use in relation to <f>Python</f>.
-        <br /> From here I decided to take the Blue Valley Caps program which
-        forced me to take on three major projects.
-        <br /> The intention wasn't to finish all three but rather to get each
-        one presentable with a finished product in mind.
-        <br /> At the end of the semester you were then forced to present in
-        front of the parents of all the students.
-        <br /> This program taught me more than just programming however it also
-        taught me professionalism in a workplace.
-        <br /> After the program ended I was able to interview with{" "}
-        <f>Mazuma Credit Union</f> to get an internship as a{" "}
-        <f>Software Developer</f>.
-        <br /> This internship gave me an understanding of the real professional
-        environment and validated my desire to work in this field.
+        I began with no prior knowledge of coding outside of basic html
+        programming and Java development. However, this was the part that
+        excited me the most since it was a whole new type of programming. I
+        didn't have any specific due date nor any features I had to add all I
+        had to do was make the bot. After asking a few friends what I should add
+        to the bot I decided to add a feature to search weather in any zip code.
+        At first, I thought this would be a simple mater but instead it ended up
+        required a lot of high-level knowledge. This led me to learn RESTful
+        programming which I also learned how to use in relation to Python. From
+        here I decided to take the Blue Valley Caps program which forced me to
+        take on three major projects. The intention wasn't to finish all three
+        but rather to get each one presentable with a finished product in mind.
+        At the end of the semester, you were then forced to present in front of
+        the parents of all the students. This program taught me more than just
+        programming however it also taught me professionalism in a workplace.
+        After the program ended, I was able to interview with Mazuma Credit
+        Union to get an internship as a Software Developer. This internship was
+        able to round out my skills really well to teach me professionalism as
+        well as programming in the workplace.
       </p2>
     </>
   );
