2971919d252a139110243fd6381e6356af0c6711
Omar-Alabdalla <omar.alabdalla04@gmail.com>
2022-11-09 14:34:39 -0600

    Readded Mobile Compatibility

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 import React from "react";
 
@@ -7,17 +8,24 @@ import "./NavBar.scss";
 function NavBar() {
   const navRef = useRef();
 
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
+
   return (
     <header>
       <nav ref={navRef}>
-        <div className="NavBar-Links">
-          <b1>Omar Alabdalla</b1>
-          <a href="/Personal-Portfolio/#/">Home</a>
-          <a href="/Personal-Portfolio/#/Projects">Projects</a>
-          <a href="/Personal-Portfolio/#/Experience">Experience</a>
-          <a href="/Personal-Portfolio/#/Contact">Contact</a>
-        </div>
+        <a href="/Home">Home</a>
+        <a href="/Projects">Projects</a>
+        <a href="/Experience">Experience</a>
+        <a href="/Contact">Contact</a>
+        <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+          <FaTimes />
+        </button>
       </nav>
+      <button className="nav-btn" onClick={showNavBar}>
+        <FaBars />
+      </button>
     </header>
   );
 }
