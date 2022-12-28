c5ba2225c3404ecb42efec86c4ab1fd92f61ae87
Omar-Alabdalla <omar.alabdalla04@gmail.com>
2022-12-28 13:51:51 -0600

    Added a little more to Contact Page.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,14 @@
 import emailjs from "@emailjs/browser";
 import { useRef } from "react";
 
+import { Container, Button, ButtonGroup } from "react-bootstrap";
+
+import {
+  AiOutlineGithub,
+  AiOutlineMail,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+
 import React from "react";
 import "./Contact.scss";
 
@@ -28,48 +36,68 @@ function Contact() {
       );
   };
   return (
-    <div class="contact-section">
-      <div className="text-zone">
-        {" "}
-        <h1>Contact Me</h1>
-        <p></p>
-        <div className="Contact-Form">
-          <form ref={refForm} onSubmit={sendEmail}>
-            <ul>
-              <li className="half">
-                <input type="text" name="name" placeholder="Name" required />
-              </li>
-              <li className="half">
-                <input type="Email" name="Email" placeholder="Email" required />
-              </li>
-              <li>
-                <input
-                  placeholder="Subject"
-                  type="text"
-                  name="Subject"
-                  required
-                />
-              </li>
-              <li>
-                <textarea
-                  placeholder="Message"
-                  name="message"
-                  required
-                ></textarea>
-              </li>
-              <li className="half">
-                <input
-                  type="submit"
-                  className="flat-button"
-                  value="SEND"
-                  required
-                />
-              </li>
-            </ul>
-          </form>
+    <Container>
+      <div class="contact-section">
+        <div className="text-zone">
+          {" "}
+          <h1>Contact Me</h1>
+          <p></p>
+          <div className="Contact-Form">
+            <form ref={refForm} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="Email"
+                    name="Email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li className="half">
+                  <input
+                    type="submit"
+                    className="flat-button"
+                    value="SEND"
+                    required
+                  />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="Socials">
+        <ButtonGroup vertical aria-label="Basic Example">
+          <Button href="https://www.linkedin.com/in/omar-alabdalla/">
+            <AiOutlineLinkedin />
+          </Button>
+          <Button href="https://github.com/Omar-Alabdalla">
+            <AiOutlineGithub />
+          </Button>
+          <Button href="mailto: omar.alabdalla04@gmail.com">
+            <AiOutlineMail />
+          </Button>
+        </ButtonGroup>
+      </div>
+    </Container>
   );
 }
 
